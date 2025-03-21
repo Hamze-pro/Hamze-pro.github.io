c000a0aab1515364e3dfa4e38c16888a5e9a92fd
Hamze <hamze.djamakayad1@dcmail.ca>
2025-03-21 08:24:39 -0400

    just listed all the images in the main js file

---
+++ Assignment4/Part2/Main.js
@@ -0,0 +1,15 @@
+const displayedImg = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+const btn = document.querySelector('.dark');
+const overlay = document.querySelector('.overlay');
+
+// List of images
+const imageList = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const altText = {
+  'pic1.jpg': 'Closeup of a blue human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white flowers',
+  'pic4.jpg': 'Ancient Egyptian wall painting',
+  'pic5.jpg': 'Butterfly on a green leaf'
+};
+
